d6ad73a9823a5717cb33174917accfbde0b2eea9
DoSerZBlock <63659999+DoSerZBlock@users.noreply.github.com>
2020-04-18 17:19:03 +0800

    nice

---
+++ dc.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+bot=commands.Bot(command_prefix='[')
+
+@bot.event
+async def on_ready():
+    print('>>Bot is online<<')
+
+bot.run(Njc4OTY3MDg5MDM3MzEyMDAx.XprEkQ.CUPn5k54iF76bY5B_BVNaGPodzs)
