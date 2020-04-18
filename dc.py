8c2ca06c0796cf930ccdbf1d115cf3eb85eeee57
DoSerZBlock <63659999+DoSerZBlock@users.noreply.github.com>
2020-04-18 21:45:02 +0800

    ping

--- dc.py
+++ dc.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
 
-bot=commands.Bot(command_prefix='DS')
+bot=commands.Bot(command_prefix='d?')
 
 @bot.event
 async def on_ready():
     print('>>Bot is online<<')
 
-bot.run(Njc4OTY3MDg5MDM3MzEyMDAx.XprEkQ.CUPn5k54iF76bY5B_BVNaGPodzs)
+@bot.command()
+async def ping(ctx):
+    await ctx.send(F'我現在的延遲有{int(bot.latency*1000)}毫秒')
+
+bot.run('Njc4OTY3MDg5MDM3MzEyMDAx.Xpr4jA.ICg6U1VBlrcXIFdki48-jxw8UNY')
